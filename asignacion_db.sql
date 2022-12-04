4be5c64c9874d50e6fe8430e2b264274eb51c8e9
EderNaylor <eduardo.s_lz@outlook.com>
2022-12-03 21:47:02 -0600

    change tables names and the statements from drop tables

--- asignacion_db.sql
+++ asignacion_db.sql
@@ -1,98 +1,98 @@
 CREATE DATABASE asignacion;
 use asignacion;
 
-DROP TABLE flokzu;
-DROP TABLE interacciones;
-DROP TABLE alumno;
-DROP TABLE crm;
-DROP TABLE agente;
-DROP TABLE asunto;
-DROP TABLE flokzu_procesos_de_retencion;
-DROP TABLE status;
-DROP TABLE nivel_de_riesgo;
-DROP TABLE supervisor;
+DROP TABLE IF EXISTS flokzu;
+DROP TABLE IF EXISTS interacciones;
+DROP TABLE IF EXISTS alumno;
+DROP TABLE IF EXISTS crm;
+DROP TABLE IF EXISTS agente;
+DROP TABLE IF EXISTS asunto;
+DROP TABLE IF EXISTS flokzu_procesos_de_retencion;
+DROP TABLE IF EXISTS status;
+DROP TABLE IF EXISTS nivel_de_riesgo;
+DROP TABLE IF EXISTS supervisor;
 
 CREATE TABLE IF NOT EXISTS supervisor (
-    clave_de_supervisor INT NOT NULL AUTO_INCREMENT,
-        PRIMARY KEY(clave_de_supervisor)
+    id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS nivel_de_riesgo (
-    clave_nivel_de_riesgo INT NOT NULL AUTO_INCREMENT,
-        PRIMARY KEY(clave_nivel_de_riesgo)
+    id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS status (
-    clave_status INT NOT NULL AUTO_INCREMENT,
-        PRIMARY KEY(clave_status)
+    id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS flokzu_procesos_de_retencion (
-    flokzu_psbaja INT NOT NULL AUTO_INCREMENT,
-        PRIMARY KEY(flokzu_psbaja)
+CREATE TABLE IF NOT EXISTS flokzu_psbaja (
+    flokzu_id INT NOT NULL UNIQUE,
+        PRIMARY KEY(flokzu_id)
 );
 CREATE TABLE IF NOT EXISTS asunto (
-    clave_de_asunto INT NOT NULL AUTO_INCREMENT,
-        PRIMARY KEY(clave_de_asunto)
+    id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS agente (
-    clave_de_agente INT NOT NULL AUTO_INCREMENT,
-        PRIMARY KEY(clave_de_agente)
+    id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS crm (
-    clave_de_llamada INT NOT NULL,
-    clave_de_asunto_id INT,
-    clave_de_agente_id INT,
-        PRIMARY KEY(clave_de_llamada),
-        FOREIGN KEY (clave_de_asunto_id) REFERENCES asunto(clave_de_asunto)
+    clave_llamada INT NOT NULL UNIQUE,
+    asunto_id INT,
+    agente_id INT,
+        PRIMARY KEY(clave_llamada),
+        FOREIGN KEY (asunto_id) REFERENCES asunto(id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-        FOREIGN KEY (clave_de_agente_id) REFERENCES agente(clave_de_agente)
+        FOREIGN KEY (agente_id) REFERENCES agente(id)
             ON UPDATE CASCADE
             ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS alumno (
     matricula INT NOT NULL,
-    clave_nivel_de_riesgo_id INT,
-    clave_de_supervisor_id INT,
-    clave_status_id INT,
-    clave_de_llamada_id INT,
+    riesgo_id INT,
+    supervisor_id INT,
+    status_id INT,
+    clave_llamada INT,
         PRIMARY KEY(matricula),
-        FOREIGN KEY(clave_nivel_de_riesgo_id) REFERENCES nivel_de_riesgo(clave_nivel_de_riesgo)
+        FOREIGN KEY(riesgo_id) REFERENCES nivel_de_riesgo(id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-        FOREIGN KEY(clave_de_supervisor_id) REFERENCES supervisor(clave_de_supervisor)
+        FOREIGN KEY(supervisor_id) REFERENCES supervisor(id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-        FOREIGN KEY(clave_status_id) REFERENCES status(clave_status)
+        FOREIGN KEY(status_id) REFERENCES status(id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-        FOREIGN KEY(clave_de_llamada_id) REFERENCES crm(clave_de_llamada)
+        FOREIGN KEY(clave_llamada) REFERENCES crm(clave_llamada)
             ON UPDATE CASCADE
             ON DELETE SET NULL                    
 );
 
 CREATE TABLE IF NOT EXISTS interacciones (
-    matricula_id INT,
-    clave_de_llamada_id INT,
-        FOREIGN KEY(matricula_id) REFERENCES alumno(matricula)
+    matricula INT,
+    clave_llamada INT,
+        FOREIGN KEY(matricula) REFERENCES alumno(matricula)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-        FOREIGN KEY(clave_de_llamada_id) REFERENCES crm(clave_de_llamada)
+        FOREIGN KEY(clave_llamada) REFERENCES crm(clave_llamada)
             ON UPDATE CASCADE
             ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS flokzu (
-    matricula_id INT,
-    flokzu_psbaja_id INT,
-        FOREIGN KEY(flokzu_psbaja_id) REFERENCES flokzu_procesos_de_retencion(flokzu_psbaja)
+    matricula INT,
+    flokzu_id INT,
+        FOREIGN KEY(flokzu_id) REFERENCES flokzu_psbaja(flokzu_id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-        FOREIGN KEY(matricula_id) REFERENCES alumno(matricula)
+        FOREIGN KEY(matricula) REFERENCES alumno(matricula)
             ON UPDATE CASCADE
             ON DELETE SET NULL
 );
