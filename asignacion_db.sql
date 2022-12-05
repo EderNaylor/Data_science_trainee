13e9acff2880684acfd16c202a32e01d3f9f8170
EderNaylor <eduardo.s_lz@outlook.com>
2022-12-04 21:27:48 -0600

    change primary and foreign key names

--- asignacion_db.sql
+++ asignacion_db.sql
@@ -42,10 +42,10 @@ CREATE TABLE IF NOT EXISTS agente (
 );
 
 CREATE TABLE IF NOT EXISTS crm (
-    llamada INT NOT NULL UNIQUE,
+    llamada_id INT NOT NULL UNIQUE,
     asunto_id INT,
     agente_id INT,
-        PRIMARY KEY(llamada),
+        PRIMARY KEY(llamada_id),
         FOREIGN KEY (asunto_id) REFERENCES asunto(id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
@@ -55,12 +55,12 @@ CREATE TABLE IF NOT EXISTS crm (
 );
 
 CREATE TABLE IF NOT EXISTS alumno (
-    matricula INT NOT NULL UNIQUE,
+    matricula_id INT NOT NULL UNIQUE,
     riesgo_id INT,
     supervisor_id INT,
     status_id INT,
     llamada_id INT,
-        PRIMARY KEY(matricula),
+        PRIMARY KEY(matricula_id),
         FOREIGN KEY(riesgo_id) REFERENCES nivel_de_riesgo(id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
@@ -70,7 +70,7 @@ CREATE TABLE IF NOT EXISTS alumno (
         FOREIGN KEY(status_id) REFERENCES status(id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-        FOREIGN KEY(llamada_id) REFERENCES crm(llamada)
+        FOREIGN KEY(llamada_id) REFERENCES crm(llamada_id)
             ON UPDATE CASCADE
             ON DELETE SET NULL                    
 );
@@ -80,10 +80,10 @@ CREATE TABLE IF NOT EXISTS interacciones (
     matricula_id int,
     llamada_id int,
         PRIMARY KEY (id),
-        FOREIGN KEY(matricula_id) REFERENCES alumno(matricula)
+        FOREIGN KEY(matricula_id) REFERENCES alumno(matricula_id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
-        FOREIGN KEY(llamada_id) REFERENCES crm(llamada)
+        FOREIGN KEY(llamada_id) REFERENCES crm(llamada_id)
             ON UPDATE CASCADE
             ON DELETE SET NULL
 );
@@ -93,7 +93,7 @@ CREATE TABLE IF NOT EXISTS flokzu (
     matricula_id INT,
     flokzu_id INT,
         PRIMARY KEY (id),
-        FOREIGN KEY(matricula_id) REFERENCES alumno(matricula)
+        FOREIGN KEY(matricula_id) REFERENCES alumno(matricula_id)
             ON UPDATE CASCADE
             ON DELETE SET NULL,
         FOREIGN KEY(flokzu_id) REFERENCES flokzu_procesos_de_retencion(flokzu)
